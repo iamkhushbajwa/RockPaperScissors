73c0876c31d8d8aca08f74463a720d6cb1db36fe
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-31 12:47:41 +0100

    Added extra tests to rock, paper and scissor win conditions

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -10,53 +10,69 @@ describe("Rock-Paper-Scissors", function() {
   });
 
   describe('winning and losing', function() {
-
     describe('rock', function() {
-
       it('should beat scissors', function() {
-
         player1.picks('rock');
         player2.picks('scissors');
         expect(game.winner()).toBe(player1);
+      });
 
+      it('should beat lizard', function(){
+        player1.picks('rock');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1)
       });
 
       it('should lose to paper', function() {
-
         player1.picks('rock');
         player2.picks('paper');
         expect(game.winner()).toBe(player2);
-
       });
 
+      it('should lose to spock', function() {
+        player1.picks('rock');
+        player2.picks('spock');
+        expect(game.winner()).toBe(player2);
+      });
     });
 
     describe('paper', function() {
-
       it('should beat rock', function() {
-
         player1.picks('paper');
         player2.picks('rock');
         expect(game.winner()).toBe(player1);
+      });
 
+      it('should beat spock',function(){
+        player1.picks('paper');
+        player2.picks('spock');
+        expect(game.winner()).toBe(player1)
       });
 
       it('should lose to scissors', function() {
-
         player1.picks('paper');
         player2.picks('scissors');
         expect(game.winner()).toBe(player2);
-
       });
 
+      it('should lose to lizard',function(){
+        player1.picks('paper');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player2)
+      });
     });
 
     describe('scissors', function() {
-
       it('should beat paper', function() {
         player1.picks('scissors');
         player2.picks('paper');
         expect(game.winner()).toBe(player1);
+      });
+
+      it('should beat lizard', function(){
+        player1.picks('scissors');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1)
       });
 
       it('should lose to rock', function() {
@@ -65,8 +81,12 @@ describe("Rock-Paper-Scissors", function() {
         expect(game.winner()).toBe(player2);
       });
 
+      it('should lose to spock',function(){
+        player1.picks('scissors');
+        player2.picks('spock');
+        expect(game.winner()).toBe(player2);
+      });
     });
-
   });
 
   describe('lizard', function(){
@@ -122,23 +142,16 @@ describe("Rock-Paper-Scissors", function() {
   });
 
   describe('draws', function() {
-
     describe('any identical picks', function() {
-
       it('should result in no winner', function() {
-
         var drawGameResults = ['rock', 'paper', 'scissors'].map(function(x) {
           player1.picks(x);
           player2.picks(x);
           return game.winner();
         });
-
         expect(drawGameResults).toEqual([null, null, null]);
-
       });
-
     });
-
   });
 
 });
