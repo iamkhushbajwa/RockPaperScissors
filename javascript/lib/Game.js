f6cfce3eae540c5dbef92b61a7f0d4566b2af348
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-01 11:33:27 +0100

    Added images, updated SpecRunner to accomodate extracted classes, added html & style

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -1,11 +1,3 @@
-function Player(name) {
-  this.name = name;
-}
-
-Player.prototype.picks = function(pick) {
-  this.pick = pick;
-}
-
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
